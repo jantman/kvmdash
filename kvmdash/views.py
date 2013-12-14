c8951769ff588cd04fbd2821919ffdd9a61aefe9
Jason Antman <jason@jasonantman.com>
2013-12-14 17:48:56 -0500

    views

--- kvmdash/views.py
+++ kvmdash/views.py
@@ -1,7 +1,12 @@
+from flask import url_for, render_template
 from kvmdash import kvmdash
 
 #url_for('static', filename='favicon.ico')
 
 @kvmdash.route('/')
+@kvmdash.route('/index')
+@kvmdash.route('/index.html')
+@kvmdash.route('/index.htm')
 def index():
-    return "hello world"
+    return render_template("index.html",
+                           title = 'kvmdash')
