9a9f458b42001e7073379f54f0e8946720b17e9a
Jason Antman <jason@jasonantman.com>
2013-12-15 12:21:23 -0500

    working on testing, but having major issues with file storage and libvirt import from contrib/ tests

--- kvmdash/contrib/tests/test_kvmdash_client.py
+++ kvmdash/contrib/tests/test_kvmdash_client.py
@@ -1,6 +1,8 @@
 from kvmdash.contrib import kvmdash_client
 import pytest
 
+pytestmark = [pytest.mark.client]
+
 def test_foo():
     foo = 1
     assert foo == "not implemented yet"
--- kvmdash/tests/test_example.py
+++ kvmdash/tests/test_example.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from kvmdash import kvmdash
+from kvmdash import config
+
+import os.path
 import pytest
 
 def test_app():
     #kvmdash = Flask(__name__)
     #from kvmdash import views
     kvmdash.testing = True
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    config.FILESTORAGE_DIR = os.path.join(os.path.abspath(os.path.join(basedir, '../')), 'data')
 
     # app.run() # this actually works here...
     with kvmdash.test_client() as client:
